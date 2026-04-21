8a5d2fe0c675efaae1272ff084ea83b3b62403d9
Vibhor Mittal <vibhormittal26@gmail.com>
2026-04-21 14:27:19 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://0454-54-226-177-158.ngrok-free.app'
+const BASE_URL = 'https://7bab-54-226-177-158.ngrok-free.app'
 
 const DEFAULT_HEADERS = {
   'ngrok-skip-browser-warning': 'true'
